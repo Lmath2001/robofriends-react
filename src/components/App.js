be634dd9db802958d961c4ae76897513e087cd5f
Lmath2001 <8950.liny.secompb@gmail.com>
2021-04-05 19:25:19 +0530

    using Redux and uploading images

--- src/components/App.js
+++ src/components/App.js
@@ -1,56 +1,48 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import CardList from '../containers/CardList';
 import SearchBox from '../containers/SearchBox';
 import Scroll from '../containers/Scroll';
 // import { robots } from './robots.js';
 //Using API instead of robots.js
 import './App.css';
-class App extends React.Component{
+const App=()=>{
 
-  constructor(){
-    super();
-    this.state={
-      robots:[],
-      searchField:'',
-    };
-  }
+  const [robots, setRobots]=useState([]);
+  const [searchField, setSearchField]=useState('');
 
-  componentDidMount(){
+  useEffect(()=>{
     fetch('https://jsonplaceholder.typicode.com/users')
     .then(response=>{
       return response.json()})
     .then(users=>{
-      return this.setState({robots:users});
+      return setRobots(users);
     })
+  },[])
+    
+  const onSearchInput=(event)=>{
+    setSearchField(event.target.value);
   }
 
-  onSearchInput=(event)=>{
-    this.setState({searchField:event.target.value})
-  }
 
-
-  render(){
-     const filteredRobots=this.state.robots.filter((robot)=>{
-        return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase());
+    const filteredRobots=robots.filter((robot)=>{
+        return robot.name.toLowerCase().includes(searchField.toLowerCase());
     });
     // console.log(filteredRobots)
-    if(this.state.robots.length===0){
+    if(robots.length===0){
       return <h1>Loading...</h1>
     }
     else{
      return(
       <div className="body-main">
         <h1>RoboFriends</h1>
-        <SearchBox searchInput={this.onSearchInput}/>
+        <SearchBox searchInput={onSearchInput}/>
         <Scroll>
           <CardList robots={filteredRobots}/>
         </Scroll>
       </div>
   );
-     }
+} }
 
-  }
-}
  
 
 
